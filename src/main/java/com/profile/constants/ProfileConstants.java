1c7eab10143e13662116afebf38ba425d47d9713
kavitha-gawarla <kavithagawarla@gmail.com>
2017-08-20 20:28:55 -0400

    added new constant

--- src/main/java/com/profile/constants/ProfileConstants.java
+++ src/main/java/com/profile/constants/ProfileConstants.java
@@ -5,5 +5,7 @@ public class ProfileConstants {
 	public static final String PHONE_TYPE_HOME="Home";
 	public static final String PHONE_TYPE_CELL="Cell";
 	public static final String PHONE_TYPE_WORK="Work";
+	
+	public static final String EMAIL_TYPE_HOME="Home Email";
 
 }
